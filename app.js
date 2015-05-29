0997881cd784614bd6996ebc858beade415c7dfc
Ernie Casilla <ecasilla@icloud.com>
2015-05-28 23:12:13 -0400

    added some collection logic :)

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-var question1 = new Question(
-  "A cat has how many whiskers, on average?",
-  "images/whiskers.jpg",
-  "24",
-  ["8","12","16", "24"]
-);
+var question1 = new Question({
+  title: "A cat has how many whiskers, on average?",
+  image: "images/whiskers.jpg",
+  answer: "24",
+  choices: ["8","12","16", "24"]
+});
 
 var question2 = new Question(
-  "A term for a group of cats is:",
-  "images/cats.jpg",
-  "clowder",
-  ["caggle","covey","clutch", "clowder"]
+  title: "A term for a group of cats is:",
+  image: "images/cats.jpg",
+  answer: "clowder",
+  choices: ["caggle","covey","clutch", "clowder"]
 );
 
 var question3 = new Question(
-  "A term for a group of kittens is:",
-  "images/kittens.jpg",
-  "kindle",
-  ["kaggle","kindle","nook", "kaboodle"]
+  title:"A term for a group of kittens is:",
+  image:"images/kittens.jpg",
+  answer:"kindle",
+  choices:["kaggle","kindle","nook", "kaboodle"]
 );
 
 var question4 = new Question(
-  "What’s it called when a cat rubs the side of its head on you or on furniture?",
-  "images/cat-rubbing.jpg",
-  "bunting",
-  ["beaning","bunting","brocking", "tagging"]
+  title:"What’s it called when a cat rubs the side of its head on you or on furniture?",
+  image:"images/cat-rubbing.jpg",
+  answer:"bunting",
+  choices:["beaning","bunting","brocking", "tagging"]
 );
 
-function Question( title, image, answer, choiceArray){
-  this.title = title;
-  this.image = image;
-  this.answer = answer;
-  this.choices = choiceArray;
+//MODEL
+function Question(options){
+  this.title = options.title;
+  this.image = options.image;
+  this.answer = options.answer;
+  this.choices = options.choiceArray;
 }
 
 
@@ -41,12 +42,12 @@ function QuestionView (collection){
 
 QuestionView.prototype.initalize = function() {
  this.nextQuestion();
- this.showQuestion(); 
+ this.showQuestion();
  this.handleQuestion();
 }
 
 QuestionView.prototype.nextQuestion = function() {
-  this.currentQuestion = this.collection.shift();
+  this.currentQuestion = this.collection.shift(); //array of questions
   this.showQuestion();
   this.handleQuestion();
 }
@@ -78,9 +79,15 @@ QuestionView.prototype.handleQuestion = function(){
 
 
 function QuestionCollection(){
-  var slice = Array.prototype.slice;
-  var args = slice.apply(arguments)
-  return args;
+  if(arguments.length === 1 && instanceof Array){
+    var arrayList = arguments[0];
+  }else{
+    var slice = Array.prototype.slice;
+    arrayList = slice.apply(arguments)
+  }
+  arrayList.map(function(elem,index){
+    return new Question(elem);
+  });
 }
 
 function rightAnswer(){
@@ -93,8 +100,9 @@ function wrongAnswer(){
 
 
 $(document).ready(function(){
+var list = [question1, question2, question4, question3];
 
-  var catCollection = new QuestionCollection(question1, question2, question4, question3);
+  var catCollection = new QuestionCollection(list);
 
   var catView = new QuestionView(catCollection);
 
